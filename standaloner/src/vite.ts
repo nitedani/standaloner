1c275df1a7e1304498e2ac2b9f5caf2daa22ddcf
rtritto <ariele.1994@gmail.com>
2026-06-29 10:51:26 +0200

    fix: remove force target es2022

--- standaloner/src/vite.ts
+++ standaloner/src/vite.ts
@@ -49,7 +49,6 @@ const standaloner = (
             external: [...builtinModules, ...builtinModules.map(m => `node:${m}`)],
           },
           build: {
-            target: 'es2022',
             minify: minify && !bundle_,
           },
         };
